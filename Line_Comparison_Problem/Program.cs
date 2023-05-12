03a65eacebfa1b21853024d69a2e1ed5647dc443
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-12 15:46:20 +0530

    Modified

--- Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Program.cs
@@ -25,16 +25,16 @@ namespace Line_Comparison_Problem
             //first line execution.
             Console.WriteLine(" Calculate the length of line using given points");
             Console.WriteLine(" Please enter the First Point (x1):");
-            float.TryParse(Console.ReadLine(), out x1);
+            x1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the Second Point (x2):");
-            float.TryParse(Console.ReadLine(), out x2);
+            x2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the Third Point (y1):");
-            float.TryParse(Console.ReadLine(), out y1);
+            y1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the Fourth Point (y2):");
-            float.TryParse(Console.ReadLine(), out y2);
+            y2 = float.Parse(Console.ReadLine());
 
             length = (float) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
@@ -44,16 +44,16 @@ namespace Line_Comparison_Problem
 
             Console.WriteLine(" Calculate the length of line using given points");
             Console.WriteLine(" Please enter the length of the second line first Point (x1_1):");
-            float.TryParse(Console.ReadLine(), out x1_1);
+            x1_1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the length of the second line second Point (x2_2):");
-            float.TryParse(Console.ReadLine(), out x2_2);
+            x2_2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the length of the second line third Point (y1_1):");
-            float.TryParse(Console.ReadLine(), out y1_1);
+            y1_1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" Please enter the length of the second line fourth Point (y2_2):");
-            float.TryParse(Console.ReadLine(), out y2_2);
+            y2_2 = float.Parse(Console.ReadLine());
 
             length2 = (float)Math.Sqrt(Math.Pow(x2_2 - x1_1, 2) + Math.Pow(y2_2 - y1_1, 2));
 
